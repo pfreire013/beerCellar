89e330b98ead1a8b4969151be180524bfcc82b62
Paulo Freire <jose.freire@devoteam.com>
2020-10-09 19:32:25 +0100

    Create a theme

---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+const theme = {
+  color: {
+    priamry: '#F6C045',
+    second: '#7B4B28',
+    white: '#FFFFFF',
+    black: '#000000',
+    lowGrey: '#F2F2F7',
+    mediumGrey: '#C7C7CC',
+    highGrey: '#8E8E93'
+  }
+}
+
+export default theme
