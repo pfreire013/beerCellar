a4ebbaf7842a2e32cba58a07c2f92534936e2ff4
Paulo Freire <jose.freire@devoteam.com>
2020-10-13 13:06:05 +0100

    Add navigation on BeerCard

--- src/components/BeerCard/BeerCard.tsx
+++ src/components/BeerCard/BeerCard.tsx
@@ -1,4 +1,7 @@
+import { useNavigation } from '@react-navigation/native'
 import React from 'react'
+import RouteNames from '../../navigation/RouteNames'
+import { HomeScreenNavigationProp } from '../../navigation/RouteTypes'
 import { Beer } from '../../redux/beers/types'
 
 import { Container, Title, Name, Category } from './styles'
@@ -7,8 +10,17 @@ interface Props {
   beer: Beer
 }
 const BeerCard: React.FC<Props> = ({ beer }) => {
+  const navigation = useNavigation<HomeScreenNavigationProp>()
+
+  function handleNavigationToDetail () {
+    navigation.navigate(RouteNames.RoutesStack.Detail, { beer: beer })
+  }
+
   return (
-    <Container>
+    <Container
+      activeOpacity={0.6}
+      onPress={handleNavigationToDetail}
+    >
       <Title>{beer.name && beer.name}</Title>
       <Name>{beer.style?.shortName}</Name>
       {/* Status icon */}
