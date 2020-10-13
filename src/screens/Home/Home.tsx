8cc2a4acdb43e624db66eaf4b91e043916733052
Paulo Freire <jose.freire@devoteam.com>
2020-10-13 13:04:04 +0100

    Fix Home layout

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,23 +1,65 @@
-import React, { useEffect } from 'react'
+import { useNavigation } from '@react-navigation/native'
+import React, { useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native'
 import { connect, ConnectedProps } from 'react-redux'
 import BeerCard from '../../components/BeerCard'
+import RouteNames from '../../navigation/RouteNames'
+import { HomeScreenNavigationProp } from '../../navigation/RouteTypes'
 import { RootState } from '../../redux'
 import { getBeers } from '../../redux/beers'
 import { Beer } from '../../redux/beers/types'
+import Services from '../../Services'
 
-import { Container, MainScroll } from './styles'
+import { Container, Header, SearchContainer, IconSearch, InputSearch, MainScroll, Title, TouchNewBeer, IconAdd, TextNewBeer, TouchMore, TextMore } from './styles'
 
-const Home = ({ beers }: { beers: Beer[]}) => {
+interface Props {
+  beers: Beer[],
+  navigationToNewBeer: () => void
+  onMore: () => void
+  onSearch: (text: string) => void
+  searchBeer: Beer[] | undefined
+  isInSearch: boolean
+}
+const Home: React.FC<Props> = ({ beers, navigationToNewBeer, onMore, onSearch, searchBeer, isInSearch }) => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <Container>
-        <MainScroll>
+        <Header>
+          <Title>Home</Title>
+          <TouchNewBeer
+            activeOpacity={0.6}
+            onPress={navigationToNewBeer}
+          >
+            <IconAdd />
+            <TextNewBeer>New Beer</TextNewBeer>
+          </TouchNewBeer>
+        </Header>
+        <SearchContainer>
+          <IconSearch />
+          <InputSearch
+            placeholder='Search the beer name...'
+            onChangeText={(text: string) => onSearch(text)}
+          />
+        </SearchContainer>
+        <MainScroll
+          bounces={false}
+          showsVerticalScrollIndicator={false}
+        >
           {
-            beers.map(beer => (
+            beers && !isInSearch && beers.map(beer => (
               <BeerCard key={beer.id} beer={beer} />
             ))
           }
+          {
+            searchBeer && isInSearch && searchBeer.map(beer => (
+              <BeerCard key={beer.id} beer={beer} />
+            ))
+          }
+          <TouchMore
+            onPress={onMore}
+          >
+            <TextMore>More</TextMore>
+          </TouchMore>
         </MainScroll>
       </Container>
     </SafeAreaView>
@@ -25,19 +67,57 @@ const Home = ({ beers }: { beers: Beer[]}) => {
 }
 
 const HomeContainer = ({ getBeers, beers }: PropsFromRedux) => {
+  const navigation = useNavigation<HomeScreenNavigationProp>()
+  const [pagination, setPagination] = useState<number>(1)
+  const [isInSearch, setIsInSearch] = useState(false)
+  const [searchBeer, setSearchBeer] = useState<Beer[] | null>()
+
   useEffect(() => {
-    getBeers()
+    setPagination(1)
+    getBeers(pagination)
   }, [])
+
+  function handleNavigationToNewBeer () {
+    navigation.navigate(RouteNames.RoutesStack.NewBeer)
+  }
+
+  function handleMore () {
+    getBeers(pagination + 1)
+    setPagination(pagination + 1)
+  }
+
+  function handleSearch (name: string) {
+    if (name === '') {
+      setIsInSearch(false)
+      setSearchBeer(null)
+    }
+    if (name !== '') {
+      setIsInSearch(true)
+    }
+    Services.getBeerSearch(name).then(res => {
+      console.log(res)
+      if (res.data.data) {
+        setSearchBeer(res.data.data)
+      } else {
+        setSearchBeer(null)
+      }
+    })
+  }
 
   return (
     <Home
       beers={beers}
+      navigationToNewBeer={handleNavigationToNewBeer}
+      onMore={handleMore}
+      onSearch={handleSearch}
+      searchBeer={searchBeer}
+      isInSearch={isInSearch}
     />
   )
 }
 
 const mapStateToProps = (state: RootState) => ({
-  beers: state.beers.beers.result
+  beers: state.beers.beers?.result
 })
 
 const connector = connect(mapStateToProps, {
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,5 +1,7 @@
 import styled from 'styled-components/native'
 import theme from '../../theme'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faPlus, faSearch } from '@fortawesome/free-solid-svg-icons'
 
 export const Container = styled.View`
   flex: 1;
@@ -10,4 +12,83 @@ export const MainScroll = styled.ScrollView`
   flex: 1;
   background: transparent;
   padding-horizontal: 24px;
+`
+
+export const Header = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  padding-horizontal: 24px;
+  align-items: center;
+  margin-vertical: 10px;
+`
+
+export const Title = styled.Text`
+  font-size: 20px;
+  color: ${theme.color.black};
+  font-weight: bold;
+`
+
+export const TouchNewBeer = styled.TouchableOpacity`
+  padding: 12px 24px;
+  background: ${theme.color.primary};
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 0px 5px 5px rgba(0, 0, 0, 0.2);
+  elevation: 4;
+  border-radius: 50px;
+`
+
+export const IconAdd = styled(FontAwesomeIcon).attrs({
+  icon: faPlus,
+  size: 20,
+  color: theme.color.white
+})`
+margin-right: 12px;
+`
+
+export const TextNewBeer = styled.Text`
+  font-size: 18px;
+  color: ${theme.color.white};
+  font-weight: bold;
+`
+
+export const TouchMore = styled.TouchableOpacity`
+  padding: 12px 24px;
+  background: ${theme.color.primary};
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 0px 5px 5px rgba(0, 0, 0, 0.2);
+  elevation: 4;
+  border-radius: 50px;
+  align-self: center;
+`
+
+export const TextMore = styled.Text`
+  font-size: 18px;
+  color: ${theme.color.secondary};
+  font-weight: bold;
+`
+
+export const SearchContainer = styled.View`
+  height: 40px;
+  background: ${theme.color.white};
+  padding-horizontal: 24px;
+  align-items: center;
+  flex-direction: row;
+  margin-bottom: 8px;
+`
+
+export const IconSearch = styled(FontAwesomeIcon).attrs({
+  icon: faSearch,
+  size: 20,
+  color: theme.color.black
+})`
+margin-right: 12px;
+`
+
+export const InputSearch = styled.TextInput`
+  color: ${theme.color.black};
+  flex: 1;
 `
