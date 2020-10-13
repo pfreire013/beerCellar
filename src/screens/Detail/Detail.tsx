e70df0199ceb98fca51be236b721e786cfb027a7
Paulo Freire <jose.freire@devoteam.com>
2020-10-13 13:05:45 +0100

    Fix Detail screen

--- src/screens/Detail/Detail.tsx
+++ src/screens/Detail/Detail.tsx
@@ -1,9 +1,72 @@
+import { useRoute } from '@react-navigation/native'
 import React from 'react'
+import TopNav from '../../components/TopNav'
+import RouteNames from '../../navigation/RouteNames'
+import { DetailScreenRouteProp } from '../../navigation/RouteTypes'
 
-import { Container } from './styles'
+import { Container, MainScroll, ImageBeer, Info, Name, Row, Card, CardTitle, CardValue, Header, StyleName, Category, Description, FoodPairings, Subtitle } from './styles'
 
 const Detail: React.FC = () => {
-  return <Container />
+  const route = useRoute<DetailScreenRouteProp>()
+  const { beer } = route.params
+  return (
+    <Container>
+      <TopNav title={RouteNames.RoutesStack.Detail} />
+      <MainScroll
+        bounces={false}
+        showsVerticalScrollIndicator={false}
+      >
+        <Header>
+          <ImageBeer />
+          <Info>
+            <Name>{beer.name}</Name>
+            <Row>
+              {
+                beer.abv && (
+                  <Card>
+                    <CardTitle>ABV:</CardTitle>
+                    <CardValue>{beer.abv}</CardValue>
+                  </Card>
+                )
+              }
+              {
+                beer.ibu && (
+                  <Card>
+                    <CardTitle>IBU:</CardTitle>
+                    <CardValue>{beer.ibu}</CardValue>
+                  </Card>
+                )
+              }
+            </Row>
+          </Info>
+        </Header>
+        <StyleName>{beer.style.name}</StyleName>
+        <Category>{beer.style.category.name}</Category>
+        <Description>
+          {beer.style.description}
+        </Description>
+
+        <Row>
+          <Card>
+            <CardTitle>Organic:</CardTitle>
+            <CardValue>{beer.isOrganic === 'N' ? 'No' : 'Yes'}</CardValue>
+          </Card>
+          <Card>
+            <CardTitle>Retired:</CardTitle>
+            <CardValue>{beer.isRetired === 'N' ? 'No' : 'Yes'}</CardValue>
+          </Card>
+        </Row>
+        {
+          beer.foodPairings && (
+            <>
+              <Subtitle>Food Pairings</Subtitle>
+              <FoodPairings>{beer.foodPairings}</FoodPairings>
+            </>
+          )
+        }
+      </MainScroll>
+    </Container>
+  )
 }
 
 export default Detail
--- src/screens/Detail/styles.ts
+++ src/screens/Detail/styles.ts
@@ -3,5 +3,92 @@ import theme from '../../theme'
 
 export const Container = styled.View`
   flex: 1;
-  background: ${theme.color.secondary}
+  background: ${theme.color.white};
+`
+
+export const MainScroll = styled.ScrollView`
+  padding-horizontal: 24px;
+`
+
+export const Header = styled.View`
+   flex-direction: row;
+   margin-bottom: 32px;
+`
+
+export const ImageBeer = styled.Image`
+  height: 90px;
+  width: 90px;
+  background: ${theme.color.lowGrey};
+  border-radius: 50px;
+  margin-right: 24px;
+`
+
+export const Info = styled.View``
+
+export const Name = styled.Text`
+  font-size: 20px;
+  color: ${theme.color.black};
+  font-weight: bold;
+  margin-bottom: 16px;
+`
+
+export const Row = styled.View`
+  flex-direction: row;
+`
+
+export const Card = styled.View`
+  padding: 8px 12px;
+  background: ${theme.color.primary};
+  border-radius: 10px;
+  margin-right: 12px;
+  flex-direction: row;
+  align-items: center;
+`
+
+export const CardTitle = styled.Text`
+  font-size: 14px;
+  color: ${theme.color.white};
+  font-weight: bold;
+  margin-right: 8px;
+`
+
+export const CardValue = styled.Text`
+  font-size: 14px;
+  color: ${theme.color.secondary};
+  font-weight: bold;
+`
+
+export const StyleName = styled.Text`
+  font-size: 18px;
+  color: ${theme.color.black};
+  font-weight: bold;
+  margin-bottom: 8px;
+`
+
+export const Category = styled.Text`
+  font-size: 16px;
+  color: ${theme.color.highGrey};
+  font-weight: bold;
+  margin-bottom: 8px;
+`
+
+export const Description = styled.Text`
+  font-size: 16px;
+  color: ${theme.color.highGrey};
+  font-weight: normal;
+  margin-bottom: 40px;
+`
+
+export const Subtitle = styled.Text`
+  font-size: 16px;
+  color: ${theme.color.black};
+  font-weight: bold;
+  margin-bottom: 8px;
+  margin-top: 20px;
+`
+
+export const FoodPairings = styled.Text`
+  font-size: 16px;
+  color: ${theme.color.highGrey};
+  font-weight: normal;
 `
